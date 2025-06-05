1df9ee99359b55452c71c6e66b38b1013ab1c7c4
BredyByte <correodemimobil@gmail.com>
2025-06-05 18:13:47 +0200

    feat: Grace is done

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int	main(void) {
-	printf("Im Grace!");
-	return 0;
-}
+// One Comment
+
+#define FILE_NAME "Grace_kid.c"
+#define SRC "#include <stdio.h>%c%c// One Comment%c%c#define FILE_NAME %cGrace_kid.c%c%c#define SRC %c%s%c%c#define WRITE_TO_FILE int main(void) { FILE *fp = fopen(FILE_NAME, %cw%c); if (!fp) return 1; fprintf(fp, SRC,10,10,10,10,34,34,10,34,SRC,34,10,34,34,10,10,10); fclose(fp); return 0;}%c%cWRITE_TO_FILE%c"
+#define WRITE_TO_FILE int main(void) { FILE *fp = fopen(FILE_NAME, "w"); if (!fp) return 1; fprintf(fp, SRC,10,10,10,10,34,34,10,34,SRC,34,10,34,34,10,10,10); fclose(fp); return 0;}
+
+WRITE_TO_FILE
