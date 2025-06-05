671cd544cd69709242b4279b0eb65bb998d806cc
BredyByte <correodemimobil@gmail.com>
2025-06-05 17:39:34 +0200

    feat: Colleen in C is done

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,7 +1,12 @@
+// Some outside comment
 #include <stdio.h>
 
+void another_function(void){}
+
 int main(void){
-char *f="#include%c<stdio.h>%c%cint%cmain(void){%cchar%c*f=%c%s%c;%cprintf(f,32,10,10,32,10,32,34,f,34,10,10,32,10,10);%creturn%c0;%c}%c";
-printf(f,32,10,10,32,10,32,34,f,34,10,10,32,10,10);
-return 0;
+	// Some inside comment
+	another_function();
+	char *f="// Some outside comment%c#include <stdio.h>%c%cvoid another_function(void){}%c%cint%cmain(void){%c	// Some inside comment%c	another_function();%c	char *f=%c%s%c;%c	printf(f,10,10,10,10,10,32,10,10,10,34,f,34,10,10,32,10,10);%c	return%c0;%c}%c";
+	printf(f,10,10,10,10,10,32,10,10,10,34,f,34,10,10,32,10,10);
+	return 0;
 }
